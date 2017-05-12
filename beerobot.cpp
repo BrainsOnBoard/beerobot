5bdd2aed9819586310ea154dd888335add51fa77
Alex Dewar <alex.dewar@gmx.co.uk>
2017-05-12 13:51:05 +0100

    initial commit

---
+++ beerobot.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * File:   beerobot.cpp
+ * Author: alex
+ *
+ * Created on 12 May 2017, 13:38
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ *
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
