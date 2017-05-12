df11b9e296a1562d6964db1808696b4ac8c1dfeb
Alex Dewar <alex.dewar@gmx.co.uk>
2017-05-12 18:14:43 +0100

    remap webcam frame to bee eye model and display not sure if this is working; output doesn't look good! may have to figure out how to display data better

--- beerobot.cpp
+++ beerobot.cpp
@@ -6,25 +6,47 @@
  */
 
 #include <cstdlib>
-
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
 
-const int KEY_Q = 113;
+#include "gigerdatacam.h"
 
+using namespace std;
 using namespace cv;
 
 int main(int, char**) {
 
+    // the size of the frame after resizing
+    Size sz(720, 480);
+
+    // to capture webcam output
     VideoCapture cap(0);
 
-    Mat fr;
+    Mat src, dst;
+    dst.create(sz, CV_32SC3);
 
+    // create x and y pixel maps
+    Mat map_x, map_y;
+    map_x.create(sz, CV_32FC1);
+    map_y.create(sz, CV_32FC1);
+    for (int i = 0; i < gdataLength; i++) {
+        for (int j = 0; j < map_x.rows; j++)
+            map_x.at<float>(j, gdata[i][0]) = gdata[i][2];
+
+        for (int j = 0; j < map_y.cols; j++)
+            map_y.at<float>(gdata[i][1], j) = gdata[i][3];
+    }
+
+    // display remapped webcam output on loop until user presses Q
+    Mat src2;
     while (1) {
-        cap >> fr;
+        cap >> src;
+        cv::resize(src, src2, sz);
 
-        imshow("camera", fr);
-        if ((waitKey(1) & 0xff) == KEY_Q)
+        remap(src2, dst, map_x, map_y, INTER_LINEAR);
+
+        imshow("camera", dst);
+        if ((char) (waitKey(1) & 0xff) == 'q')
             break;
     }
 
