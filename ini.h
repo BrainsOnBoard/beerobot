fc7a2f1616ab273d714272f9db33be50a1c24b75
Alex Dewar <alex.dewar@gmx.co.uk>
2017-05-22 21:38:32 +0100

    use c++ object for ini files instead

--- ini.h
+++ ini.h
@@ -14,6 +14,83 @@ using namespace std;
 
 const char* INI_FILE = "beerobot.ini";
 
+class CamParams {
+private:
+
+    static void get(const dictionary *ini, int &val, const char *str) {
+        val = iniparser_getint(ini, str, val);
+    }
+
+    static void get(const dictionary *ini, double &val, const char *str) {
+        val = iniparser_getdouble(ini, str, val);
+    }
+
+public:
+    int src_wd = 1280;
+    int src_ht = 720;
+    int dst_wd = 1280;
+    int dst_ht = 300;
+    double cent_x = 0.5;
+    double cent_y = 0.5;
+    double r_inner = 0.1;
+    double r_outer = 0.5;
+
+    void read() {
+        dictionary *ini = iniparser_load(INI_FILE);
+        if (!ini) {
+            cout << "Could not find " << INI_FILE;
+            return;
+        }
+
+        cout << "Reading settings from " << INI_FILE << endl;
+
+        get(ini, this->src_wd, "camera:width");
+        get(ini, this->src_ht, "camera:height");
+        get(ini, this->dst_wd, "unwrap:width");
+        get(ini, this->dst_ht, "unwrap:height");
+        get(ini, this->cent_x, "unwrap:cent-x");
+        get(ini, this->cent_y, "unwrap:cent-y");
+        get(ini, this->r_inner, "unwrap:r-inner");
+        get(ini, this->r_outer, "unwrap:r-outer");
+
+        // free memory
+        iniparser_freedict(ini);
+    }
+
+    void write() {
+        cout << "Writing settings to " << INI_FILE << endl;
+
+        FILE *ini = fopen(INI_FILE, "w");
+        if (!ini) {
+            cerr << "Error: Could not create file " << INI_FILE << endl;
+            return;
+        }
+
+        fprintf(ini,
+                "[camera]\n"
+                "width  = %d\n"
+                "height = %d\n\n"
+                "[unwrap]\n"
+                "width   = %d\n"
+                "height  = %d\n"
+                "cent-x  = %g\n"
+                "cent-y  = %g\n"
+                "r-inner = %g\n"
+                "r-outer = %g\n",
+                this->src_wd,
+                this->src_ht,
+                this->dst_wd,
+                this->dst_ht,
+                this->cent_x,
+                this->cent_y,
+                this->r_inner,
+                this->r_outer
+                );
+
+        fclose(ini);
+    }
+};
+
 struct params {
     int src_wd = 1280;
     int src_ht = 720;
@@ -24,63 +101,6 @@ struct params {
     double r_inner = 0.1;
     double r_outer = 0.5;
 };
-
-void get(const dictionary *ini, int &val, const char *str) {
-    val = iniparser_getint(ini, str, val);
-}
-
-void get(const dictionary *ini, double &val, const char *str) {
-    val = iniparser_getdouble(ini, str, val);
-}
-
-void read_params(params &p) {
-    dictionary *ini = iniparser_load(INI_FILE);
-    if (ini) {
-        get(ini, p.src_wd, "camera:width");
-        get(ini, p.src_ht, "camera:height");
-
-        get(ini, p.dst_wd, "unwrap:width");
-        get(ini, p.dst_ht, "unwrap:height");
-        get(ini, p.cent_x, "unwrap:cent-x");
-        get(ini, p.cent_y, "unwrap:cent-y");
-        get(ini, p.r_inner, "unwrap:r-inner");
-        get(ini, p.r_outer, "unwrap:r-outer");
-
-        // free memory
-        iniparser_freedict(ini);
-    }
-}
-
-void write_params(params &p) {
-    FILE *ini = fopen(INI_FILE, "w");
-    if (!ini) {
-        cerr << "Error: Could not create file " << INI_FILE << endl;
-        return;
-    }
-
-    fprintf(ini,
-            "[camera]\n"
-            "width  = %d\n"
-            "height = %d\n\n"
-            "[unwrap]\n"
-            "width   = %d\n"
-            "height  = %d\n"
-            "cent-x  = %g\n"
-            "cent-y  = %g\n"
-            "r-inner = %g\n"
-            "r-outer = %g\n",
-            p.src_wd,
-            p.src_ht,
-            p.dst_wd,
-            p.dst_ht,
-            p.cent_x,
-            p.cent_y,
-            p.r_inner,
-            p.r_outer
-            );
-
-    fclose(ini);
-}
 
 #endif /* INI_H */
 
