9346cb03d7793423afa1aae45abc91c0eab6d22e
Alex Dewar <a.dewar@sussex.ac.uk>
2017-05-22 21:48:43 +0100

    revert removal of ENABLE_CONTROLLER flag in beerobot.cpp

--- beerobot.cpp
+++ beerobot.cpp
@@ -12,17 +12,24 @@
 #include "beeeye.h"
 #include "xboxrobot.h"
 
+#define ENABLE_CONTROLLER
+
 int main(int, char**) {
 
     thread tcamera(run_camera); // thread for displaying camera output on screen
+
+#ifdef ENABLE_CONTROLLER
     thread tcontroller(run_controller); // thread for handling controller button presses
+#endif
 
     // wait for the camera thread to finish
     tcamera.join();
 
+#ifdef ENABLE_CONTROLLER
     // camera thread ends when user quits, so we must now stop controller thread
     do_run_controller = false;
     tcontroller.join(); // wait for thread to finish
+#endif
 
     return 0;
 }
