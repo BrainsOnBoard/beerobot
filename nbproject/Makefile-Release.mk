d717a5c233c12d70afeb5bbd7c1453619189be78
Alex Dewar <a.dewar@sussex.ac.uk>
2017-09-01 10:55:18 +0100

    tweak makefile/netbeans make config

--- nbproject/Makefile-Release.mk
+++ nbproject/Makefile-Release.mk
@@ -42,8 +42,8 @@ OBJECTFILES= \
 CFLAGS=
 
 # CC Compiler Flags
-CCFLAGS=-std=c++11
-CXXFLAGS=-std=c++11
+CCFLAGS=-pthread -std=c++11
+CXXFLAGS=-pthread -std=c++11
 
 # Fortran Compiler Flags
 FFLAGS=
