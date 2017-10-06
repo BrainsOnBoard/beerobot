fe010cfa212253d0981232519258e854cd014783
Alex Dewar <a.dewar@sussex.ac.uk>
2017-10-06 19:02:26 +0100

    scripts to run server and client

---
+++ client.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# script to start http client locally
+
+./dist/Release/GNU-Linux/beerobot --controller viewer 192.168.1.3
---
+++ server.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# script to start robot server
+
+echo 'cd beerobot && nohup dist/Release/GNU-Linux/beerobot' | ssh ubuntu@192.168.1.3
