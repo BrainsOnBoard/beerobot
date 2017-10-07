0d56b02556e9ec86a5bcf7439dd23f79d40db0c3
Alex Dewar <a.dewar@sussex.ac.uk>
2017-10-07 16:15:11 +0100

    Revert "run client program in infinite loop to reopen automatically after"
    This reverts commit 4d789502c3cb1da92736cf7de4ee4ee021632cb1.

--- client.sh
+++ client.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 # script to start http client locally
 
-while true; do
-	./dist/Release/GNU-Linux/beerobot --controller viewer 192.168.1.3
-done
+./dist/Release/GNU-Linux/beerobot --controller viewer 192.168.1.3
