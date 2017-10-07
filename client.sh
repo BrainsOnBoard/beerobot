4d789502c3cb1da92736cf7de4ee4ee021632cb1
Alex Dewar <a.dewar@sussex.ac.uk>
2017-10-07 16:15:11 +0100

    run client program in infinite loop to reopen automatically after crashes this is a horrible hack

--- client.sh
+++ client.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 # script to start http client locally
 
-./dist/Release/GNU-Linux/beerobot --controller viewer 192.168.1.3
+while true; do
+	./dist/Release/GNU-Linux/beerobot --controller viewer 192.168.1.3
+done
