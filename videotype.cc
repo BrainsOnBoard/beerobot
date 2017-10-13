0f8ac2b1389f7a81761f7961e39b40f97a58fac4
Alex Dewar <a.dewar@sussex.ac.uk>
2017-10-13 17:01:03 +0100

    select correct ini file depending on usb camera

--- videotype.cc
+++ videotype.cc
@@ -17,13 +17,14 @@
 using namespace std;
 
 /* get the number for a camera with a given name (-1 if not found) */
-int get_camera_by_name(const string name)
+int get_camera_by_name(const string name0, const string name1, int &selected)
 {
     char cname[4096];
 
-    // iterate through devices video0, video1 etc. reading the device name from sysfs
+    // iterate through devices /dev/video0, /dev/video1 etc. reading the device name from sysfs
     // until the correct device is found
     int defcam = -1;
+    int cam1 = -1;
     for (int i = 0; i < 10; i++) {
         string vfn = "/sys/class/video4linux/video" + to_string(i) + "/name";
         ifstream file(vfn, ios::in);
@@ -35,30 +36,46 @@ int get_camera_by_name(const string name)
         cname[file.gcount() - 1] = 0; // delete the last char, which is always newline
         file.close();
 
-        if (string(cname).compare(0, name.length(), name) == 0) { // we've found the correct device
+        if (string(cname).compare(0, name0.length(), name0) == 0) { // we've found the correct device
+            selected = 0;
             return i;
+        }
+        if (string(cname).compare(name1) == 0) {
+            cam1 = i;
         }
 
         defcam = i;
     }
 
+    if (cam1 >= 0) {
+        selected = 1;
+        return cam1;
+    }
+
     if (defcam == -1) {
-        cerr << "Error: Could not find video device " << name << " and there are no other cameras attached" << endl;
+        cerr << "Error: Could not find video device " << name0 << " or " << name1 << " and there are no other cameras attached" << endl;
         exit(1);
     }
 
-    cerr << "Warning: Could not find video device " << name << ". Using default instead." << endl;
+    cerr << "Warning: Could not find video device " << name0 << " or " << name1 << ". Using default instead." << endl;
     return defcam;
 }
 
 vid_t* get_pixpro_usb()
 {
     vid_t* vid = new vid_t;
-    vid->dev_int = get_camera_by_name("PIXPRO SP360 4K");
+    int sel;
+    vid->dev_int = get_camera_by_name("PIXPRO SP360 4K", "USB 2.0 Camera", sel);
     vid->dev_char = NULL;
-    vid->width = 1440;
-    vid->height = 1440;
-    vid->ini_file = "beerobot_usb.ini";
+    if (sel == 0) {
+        vid->width = 1440;
+        vid->height = 1440;
+        vid->ini_file = "beerobot_pixpro_usb.ini";
+    } else {
+        vid->width = 1280;
+        vid->height = 720;
+        vid->ini_file = "beerobot_webcam_usb.ini";
+    }
     return vid;
 }
 
@@ -68,5 +85,5 @@ vid_t* get_pixpro_wifi()
     vid->dev_char = "http://172.16.0.254:9176/;dummy_parameter=bee.mjpg";
     vid->width = 1024;
     vid->height = 1024;
-    vid->ini_file = "beerobot_wifi.ini";
+    vid->ini_file = "beerobot_pixpro_wifi.ini";
 }
